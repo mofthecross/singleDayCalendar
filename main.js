e7c1375e7ea1b93b9d9767c49081ef0b2bd70870
mofthecross <mofthecross@gmail.com>
2016-12-18 19:14:08 -0800

    Implement checkCollision for 2 comparing 2 events;

--- main.js
+++ main.js
@@ -27,9 +27,20 @@ class SingleDayView {
     }
     this.events = arrayOfEvents;
   }
+
+  checkCollision(event1, event2) {
+    if (event1.start < event2.start && event1.end <= event2.start) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
 }
 
 const testcase = [{start: 100, end: 72},  {start: 0, end: 30}, {start: 0, end: 60}, {start:30, end: 90}, {start: 360, end: 700}, {start: 360, end: 700}]//, {start: 30, end: 150}, {start: 540, end: 600}, {start: 610, end: 670} ]
 let singleDayViewTest = new SingleDayView('events');
 singleDayViewTest.addEvents(testcase);
-console.log(singleDayViewTest);
+console.log(singleDayViewTest.checkCollision( {start: 0, end: 30}, {start:60, end: 90}))
+
+//console.log(singleDayViewTest);
