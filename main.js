9b80a110f547f06f740eecb8521643973707590a
mofthecross <mofthecross@gmail.com>
2016-12-18 20:01:40 -0800

    Implement Collisions datastructure, handleCollision method on SingleDayView,

--- main.js
+++ main.js
@@ -4,9 +4,21 @@ class Event {
     this.start = event.start;
     this.end = event.end;
     this.collisions = null;
-    this.coordinates = { width: null, x: null, y: null }
+    this.coordinates = { x: null, y: null, width: null, height: null }
     this.eventName = eventName || 'Sample item';
     this.eventLocation = eventLocation || 'sample location';
+  }
+}
+class Collisions {
+  constructor(){
+    this.storage = {};
+    this.length = 0;
+    this.resolved = false;
+  }
+
+  addCollision(event) {
+    this.storage[event.id] = event;
+    this.length++;
   }
 }
 
@@ -35,12 +47,37 @@ class SingleDayView {
       return true;
     }
   }
+  handleCollisions() {
+    for (let current = 0; current < this.events.length; current++) {
+
+      let next = current + 1;
+      //compare two events by passing it into checkCollision method;
+      if (next < this.events.length && this.checkCollision(this.events[current], this.events[next])) {
+        let conflicts = this.events[current].collisions;
+
+        //if current event has already collided with a previous event add next event to all collisions;
+        //so that each event that belongs to the one collision group has a pointer to the same Collision datastructure;
+        if (this.events[current].collisions !== null && this.events[next].collisions === null) {
+          conflicts.addCollision(this.events[next]);
+        } else {
+          //instantiate a new collision datastructure;
+          conflicts = new Collisions();
+          conflicts.addCollision(this.events[current]);
+          conflicts.addCollision(this.events[next]);
+          this.events[current].collisions = conflicts;
+        }
+        //assign collision data structure to the next event;
+        this.events[next].collisions = conflicts;
+      }
+    }
+    return this.events;
+  }
 
 }
 
 const testcase = [{start: 100, end: 72},  {start: 0, end: 30}, {start: 0, end: 60}, {start:30, end: 90}, {start: 360, end: 700}, {start: 360, end: 700}]//, {start: 30, end: 150}, {start: 540, end: 600}, {start: 610, end: 670} ]
 let singleDayViewTest = new SingleDayView('events');
 singleDayViewTest.addEvents(testcase);
-console.log(singleDayViewTest.checkCollision( {start: 0, end: 30}, {start:60, end: 90}))
+singleDayViewTest.handleCollisions()
 
-//console.log(singleDayViewTest);
+console.log(singleDayViewTest);
